61cde25753d51e7da1759d0d62e8fcbf48949dcd
Gennadiy <relect@bk.ru>
2024-11-13 13:58:44 +0300

    fix

--- src/main/java/com/javacode/wallet/repository/WalletRepo.java
+++ src/main/java/com/javacode/wallet/repository/WalletRepo.java
@@ -2,9 +2,6 @@ package com.javacode.wallet.repository;
 
 import com.javacode.wallet.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.UUID;
 
 public interface WalletRepo extends JpaRepository<Wallet, String> {
 }
